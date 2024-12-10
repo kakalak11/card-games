f82932502718f82caca380b729734cddd6d6c388
kakalak33 <tu.tran@enotion.io>
2024-12-10 21:56:30 +0700

    - update build

--- build/web-desktop/cocos-js/spine-3e0daee9.js
+++ build/web-desktop/cocos-js/spine-3e0daee9.js
@@ -1,1 +1,11 @@
-System.register([],(function(e){"use strict";return{execute:function(){e("default","assets/spine-a0005c42.wasm")}}}));
+System.register([], (function (exports, module) {
+	'use strict';
+	return {
+		execute: (function () {
+
+			var spine = exports('default', 'assets/spine-a0005c42.wasm');
+
+		})
+	};
+}));
+//# sourceMappingURL=spine-3e0daee9.js.map
